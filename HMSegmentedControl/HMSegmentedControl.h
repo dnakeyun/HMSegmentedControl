2a56f3a69a14d6e67783c0e5053a0a75041b5960
ChenQun <cqcool@icloud>
2022-07-27 11:14:24 +0800

    新增选中索引方法

--- HMSegmentedControl/HMSegmentedControl.h
+++ HMSegmentedControl/HMSegmentedControl.h
@@ -262,7 +262,7 @@ typedef NS_ENUM(NSInteger, HMSegmentedControlImagePosition) {
 - (void)setSelectedSegmentIndex:(NSUInteger)index animated:(BOOL)animated;
 - (void)setIndexChangeBlock:(IndexChangeBlock)indexChangeBlock;
 - (void)setTitleFormatter:(HMTitleFormatterBlock)titleFormatter;
-
+- (void)setSelectedSegmentIndex:(NSUInteger)index animated:(BOOL)animated notify:(BOOL)notify;
 @end
 
 NS_ASSUME_NONNULL_END
